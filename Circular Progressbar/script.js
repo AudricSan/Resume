2a96edd2e64cd4f78e16d3c251bb5a48176bf022
Audric Rosier <audricrosier@gmail.com>
2022-07-11 15:48:43 +0200

    css update

--- Circular Progressbar/script.js
+++ Circular Progressbar/script.js
@@ -9,7 +9,7 @@ let progress = setInterval(() => {
   progressValue++;
   valueContainer.textContent = `${progressValue}%`;
   progressBar.style.background = `conic-gradient(
-      #4d5bf9 ${progressValue * 3.6}deg,
+      #526886 ${progressValue * 3.6}deg,
       #cadcff ${progressValue * 3.6}deg
   )`;
   if (progressValue == progressEndValue) {
